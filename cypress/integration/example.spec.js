2a5bc202952f4dbf0ec8962fd4a6611610599866
jsclarridge <2491291+jsclarridge@users.noreply.github.com>
2020-06-09 16:33:11 -0700

    Add docker-compose, basic cypress test

---
+++ cypress/integration/example.spec.js
@@ -0,0 +1,11 @@
+describe('The Home Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/') // change URL to match your dev URL
+  })
+})
+
+# describe('Nonexistent page', () => {
+#   it('successfully loads', () => {
+#     cy.visit('/foo') // change URL to match your dev URL
+#   })
+# })
